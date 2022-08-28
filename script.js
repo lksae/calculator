41856bd9d967ee5d8958671c152621233f975cc4
Lksae <matthiasschmitz21@gmail.com>
2022-08-28 11:07:00 +0200

    Finish logic to add numbers to display

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue = 0;
+
 function add(number1, number2) {
     return number1 + number2
 }
@@ -33,3 +35,49 @@ function operate(operator, number1, number2) {
             break;
     }
 }
+
+function getAllNumbers() {
+    const numbers = document.querySelectorAll(".number");
+
+    numbers.forEach(function(number) {
+        number.addEventListener('click', function(n) {
+            if(displayValue == 0) {
+                displayValue = n.target.innerHTML;
+            } else {
+                displayValue += n.target.innerHTML;
+            }
+            
+            changeDisplayValue(displayValue);
+            
+            
+        });
+    }
+    )
+}
+
+function changeDisplayValue(number) {
+    const display = document.querySelector('#display');
+    display.textContent = number;
+}
+
+function setClear() {
+    const clear = document.querySelector('.clear');
+    clear.addEventListener('click',function() {
+        changeDisplayValue(0);
+        displayValue = 0;
+    })
+}
+
+function addEventListenerAtOperators(){
+    const operators = document.querySelectorAll('.operator');
+    operators.forEach(function(operator) {
+        operator.addEventListener('click', function(o) {
+            if (o.target.innerHTML == "=") {
+                alert("=");
+            }
+        })
+    })
+}
+getAllNumbers()
+setClear()
+addEventListenerAtOperators()
